4f1ce4913acbec87f9c2d52ed0d67c58dfb88708
Howard <howarde8@gmail.com>
2020-12-18 12:03:20 +0800

    v0.4.0 Ref and split specs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const app = require('express')();
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs')
-const swaggerDocs = YAML.load('./swagger.yml')
+const YAML = require('yamljs');
+const swaggerIndex = YAML.load('swagger.yml');
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+const RefParser = require('json-schema-ref-parser');
+RefParser.dereference(swaggerIndex).then((swaggerDocs) => {
+  app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+  });
 });
